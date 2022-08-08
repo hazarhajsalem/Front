952cb4e632aedbc12e3acc810d56e291f8a9d1c6
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-08 14:38:58 +0100

    feat: add `Login` component  in `src` folder

---
+++ src/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export function Login() {
+  return (
+    <div className="App">
+      Login page
+    </div>
+  );
+}
+
