2bdb9d481142c304b6d995807ca9c629a6f048e2
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-16 10:35:03 +0100

    fix: fix `Login` component content

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,35 +1,57 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useNavigate} from "react-router-dom"
 
 
 
 export  function Login() {
   const [email, setUser] = useState('');
-  const [psw, setPsw] = useState('') ;
+  const [password, setPsw] = useState('') ;  
+  const navigate = useNavigate();
 
   const register = () => {
-    axios.post("http://localhost:5000/api/v1/addUser", {
+    if(email!=='' && password!=='')
+   { axios.post("http://localhost:5000/api/v1/users", {
       email: email,
-      password : psw,
+      password : password,
     }).then((Response) => {
-      console.log(Response);
-    } )
+      if(Response.data.errors === true)
+      {
+        navigate("/Livre");
+      }
+      else
+      {
+        toast(Response.data.data)
+      }
+
+    } )}
+    else{
+      toast("the values shouldn't be null !!!")
+    }
   };
 
   return (  
     <div className="App">
         <h1 className="text">Login</h1>    
-        <form className="form">
-         <label className='label'>Email</label>
-            <input type="email" className="email" minLength={8} required 
+        <div className="form">
+            <input type="email" className="email" minLength={8} required placeholder="Email" 
+             onFocus={(e) => 
+              e.target.placeholder=""}
+            onBlur={(e) => 
+              e.target.placeholder="Email"}
               onChange={(e) => {
                 setUser(e.target.value)
               }}
             ></input>
             <br></br>
             <br></br>
-            <label className='label'>Password</label>  
-            <input type="password" minLength={8} required className="psw"
+            <input type="password" minLength={8} required className="psw" placeholder="Password" 
+             onFocus={(e) => 
+              e.target.placeholder=""}
+            onBlur={(e) => 
+              e.target.placeholder="Password"}
             onChange={(e) => {
               setPsw(e.target.value)
             }}
@@ -37,7 +59,8 @@ export  function Login() {
             <br></br>
             <br></br>
             <button className="button" onClick={register}>Login</button>
-        </form>
+            <ToastContainer />
+        </div>
     </div>
   );
 }
