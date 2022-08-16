33330dab3eb0c5295ca3a0d827a186168f7391b5
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-16 10:43:51 +0100

    fix : add navigation in `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Livre } from './Livre';
+import {Login} from './Login'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route  path="/" element={<Login/>}/>
+          <Route  path="/Livre" element={<Livre/>}/>
+        </Routes>
+      </Router>
+      </>
+    
   );
 }
 
 export default App;
+ 
+
