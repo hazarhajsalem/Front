66f83c10f7789d0f1bc547b4b1bf56248123bf7c
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-16 10:35:59 +0100

    feat: add `Livre` component in `src` folder

---
+++ src/Livre.tsx
@@ -0,0 +1,107 @@
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const baseURL ="http://localhost:5000/api/v1/add";
+
+export  function Livre() {
+
+const [isbn, setIsbn] = useState('');
+const [titre, setTitre] = useState('') ;
+const [auteur, setAuteur] = useState('');
+const [categorie, setCategorie] = useState('') ;
+
+useEffect(() => {
+  axios.get(`${baseURL}/1`).then((response) => {
+    setIsbn(response.data);
+  });
+}, []);
+
+const add = () => {
+  if(isbn.length > 7 && titre!=='' && auteur!=='' && categorie!=='' )
+  {
+      axios.post(baseURL, {
+        isbn : isbn,
+        titre : titre,
+        auteur : auteur,
+        categorie: categorie,
+      }).then((response) => {
+        if (response.data.data){
+              if(response.data.data ==='ISBN exist !!')
+            {    toast('ISBN exist !!')
+                  
+            }        
+              else 
+            { 
+                    toast('Add successfully ^_^')
+            }
+            }
+      } )
+  }
+  else
+  {
+    if(isbn.length < 8)
+    toast(" Length of ISBN should be > 7")
+    else
+    {
+        toast("the values shouldn't be null")
+    }
+  }
+};
+  return (  
+    <div className="App">
+        <div className="form">
+            <h1 className="text">Create Book</h1> 
+            <input type="text" className="input" placeholder="Title" required 
+             onFocus={(e) =>
+               e.target.placeholder=""} 
+             onBlur={(e) =>
+               e.target.placeholder="Title"}  
+             onChange={(e) => {
+               setTitre(e.target.value)
+            }}
+            />
+            <br></br>
+            <br></br>
+            <input type="text" className="input" placeholder="Author" required 
+              onFocus={(e) => 
+                e.target.placeholder=""} 
+              onBlur={(e) => 
+                e.target.placeholder="Author"} 
+              onChange={(e) => {
+                setAuteur(e.target.value)
+              }}
+            />
+            <br></br>
+            <br></br>
+            <input type="text" className="input" placeholder="Genre" required 
+              onFocus={(e) => 
+                e.target.placeholder=""}
+              onBlur={(e) => 
+                e.target.placeholder="Genre"}
+              onChange={(e) => {
+                setCategorie(e.target.value)
+            }}
+            />
+            <br></br>
+            <br></br>
+            <input type="text" className="input" placeholder="ISBN" required 
+              onFocus={(e) => 
+                e.target.placeholder=""} 
+              onBlur={(e) =>
+                 e.target.placeholder="ISBN"} 
+              onChange={(e) => {
+                setIsbn(e.target.value)
+              }}
+            />
+            <br></br>
+            <br></br>
+            <button className="button" onClick={add}>Add book</button>
+            <ToastContainer />
+        </div>
+    </div>
+  );
+}
+
