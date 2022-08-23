8800f29dc8b1b1ddc36160cccebbd9c3e32895de
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-23 17:21:35 +0100

    fix: fix `Livre` component contents

--- src/Livre.tsx
+++ src/Livre.tsx
@@ -2,16 +2,31 @@ import React, {useEffect, useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
-const baseURL ="http://localhost:5000/api/v1/add";
+import book from "./books.png";
+import logout from "./logout.png";
+import photo from "./userlogo.png";
+import {useNavigate} from "react-router-dom"
 
 export  function Livre() {
 
+const baseURL ="http://localhost:5000/api/v1/add";
 const [isbn, setIsbn] = useState('');
 const [titre, setTitre] = useState('') ;
 const [auteur, setAuteur] = useState('');
 const [categorie, setCategorie] = useState('') ;
+const[open, setOpen] = useState(false);
+const navigate = useNavigate();
+
+const navbook =() =>
+{
+    navigate("/Books");
+}
+
+const navlogout =() =>
+{
+    navigate("/Login");
+}
+
 
 useEffect(() => {
   axios.get(`${baseURL}/1`).then((response) => {
@@ -101,7 +116,32 @@ const add = () => {
             <button className="button" onClick={add}>Add book</button>
             <ToastContainer />
         </div>
-    </div>
+
+        <div className='container'>
+          <div className='trigger' onClick={()=>{setOpen(!open)}}>
+          <img src ={photo}  alt=""/> 
+          </div>
+          <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
+              <h3>MY ACCOUNT</h3>
+              <ul>
+                <DropdownItem image={book} text={"Books"} click={navbook}/> 
+                <DropdownItem image={logout} text={"Logout"} click={navlogout}/>
+              </ul>
+          </div>
+        </div>
+      </div>
   );
 }
+
+function DropdownItem(props : any)
+{
+  return( 
+    <li className="dropdownItem">
+      <img src={props.image} alt="" width="20px"></img> &nbsp;&nbsp;
+      <text onClick={props.click}> {props.text} </text>     
+    </li>
+  );
+}
+
+
 
