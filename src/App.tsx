3c381719e1ec553eff83d918a796f846ea4fe58c
hazarhajsalem <hazarhaj2000@gmail.com>
2022-08-23 17:35:42 +0100

    fix: fix `App` component contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import { Livre } from './Livre';
-import {Login} from './Login'
+import {Login} from './Login';
+import { Books } from './Books';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+
+
+import "./App.css";
+
 
 function App() {
  
-  return (
+
+
+   return (
     <>
       <Router>
         <Routes>
           <Route  path="/" element={<Login/>}/>
           <Route  path="/Livre" element={<Livre/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/Books" element={<Books/>}/>
         </Routes>
       </Router>
       </>
-    
   );
 }
 
-export default App;
- 
 
+export default App;
